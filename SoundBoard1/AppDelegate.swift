2f0d03bbd502e810dcb17fe0cd66c9bc7e873787
playground101 <playgrounddfw@gmail.com>
2018-09-24 20:46:29 -0500

    Initial Commit

---
+++ SoundBoard1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoundBoard1
+//
+//  Created by Mekhala Vithala on 9/24/18.
+//  Copyright © 2018 playground. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SoundBoard1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundBoard1
+//
+//  Created by Mekhala Vithala on 9/24/18.
+//  Copyright © 2018 playground. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
